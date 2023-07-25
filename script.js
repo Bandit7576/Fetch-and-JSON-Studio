222575d59891773d0abd5149850c92ccb3b68d24
Wilber Hasty <wilberhasty75@gmail.com>
2023-07-25 15:16:37 -0500

    bonus mission complete

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 // TODO: add code here
 window.addEventListener("load", function() {
-    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
     .then(function(response) {
         response.json().then(function(json) {
-            const astronaut = document.getElementById("container");
-            let index = 0;
-            console.log(json.length)
-            while (index < json.length) {
-                astronaut.innerHTML += `
-                <div class="astronaut">
-                  <div class="bio">
-                    <h3> ${json[index].firstName}  ${json[index].lastName} </h3>
-                    <ul>
-                      <li>Hours in space: ${json[index].hoursInSpace} </li>
-                      <li>Active: ${json[index].active}</li>
-                     <li>Skills: ${json[index].skills}</li>
-                    </ul>
-                  </div>
-                  <img class="avatar" src=${json[index].picture} height=200></img>
-                </div>
+          json.sort(function (a, b) {
+            return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+          });
+          const  count = document.getElementById("count");
+          count.innerHTML = `There are ${json.length} astronauts in your list.`;
+          const astronaut = document.getElementById("container");
+          let index = 0;
+          while (index < json.length) {
+            let activeClass = json[index].active ? "active" : "";
+            console.log(activeClass);
+            astronaut.innerHTML += `
+            <div class="astronaut">
+              <div class="bio">
+                  <h3> ${json[index].firstName}  ${json[index].lastName} </h3>
+                <ul>
+                  <li>Hours in space: ${json[index].hoursInSpace} </li>
+                  <li class="${activeClass}">Active: ${json[index].active}</li>
+                  <li>Skills: ${json[index].skills}</li>
+                </ul>
+              </div>
+              <img class="avatar" src=${json[index].picture} height=200></img>
+            </div>
 
-                `;
-            index = index + 1;    
-        
-            }
+            `;
+          index = index + 1;    
+      
+          }
         });
     });
 
